6080bc3cb88faa789cf733ae246f4e535bbd3e83
shruthan650 <shruthan41@gmail.com>
2026-09-20 17:59:07 +0530

    CrossOrigin for frontend access

--- src/main/java/com/shruthan/musicplayer/config/SecurityConfig.java
+++ src/main/java/com/shruthan/musicplayer/config/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.shruthan.musicplayer.config;
 
-
+import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,28 +11,28 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.shruthan.musicplayer.security.JWTFilter;
 import com.shruthan.musicplayer.service.UserService;
-
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
-	
+
 	private final UserService userService;
 	private final JWTFilter jwtFilter;
 	private final PasswordEncoder passwordEncoder;
-	
-	public SecurityConfig(
-			UserService userService,
-			JWTFilter jwtFilter,
-			PasswordEncoder passwordEncoder) {
-		
+
+	public SecurityConfig(UserService userService, JWTFilter jwtFilter, PasswordEncoder passwordEncoder) {
+
 		this.jwtFilter = jwtFilter;
 		this.passwordEncoder = passwordEncoder;
 		this.userService = userService;
@@ -41,26 +41,36 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) {
 
-		return http.csrf(customizer -> customizer.disable())
-				.authorizeHttpRequests(
-						authorize -> authorize.requestMatchers(
-								"/api/login",
-								"/api/register"
-						)
-						.permitAll()
-						.anyRequest()
-						.authenticated()
-				)
-				.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
-				.build();
+		return http.cors(customizer -> customizer.disable())
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.authorizeHttpRequests(authorize -> authorize.requestMatchers("/api/login", "/api/register").permitAll()
+						.anyRequest().authenticated())
+				.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class).build();
 	}
-	
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+
+		configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+
+		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+		configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+		source.registerCorsConfiguration("/**", configuration);
+
+		return source;
+	}
+
 	@Bean
 	public AuthenticationProvider authenticationProvider() {
-		
+
 		DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider(userService);
 		daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
-		
+
 		return daoAuthenticationProvider;
 	}
 
